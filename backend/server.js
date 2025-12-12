bbe16742fc2e56ba402ad807bfcdfb4a13420f23
Rahcurl <rsharma194@rku.ac.in>
2025-12-12 09:59:49 +0545

    API endpoint checkin success

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hey from server")
+import express from "express"
+
+const  app = express()
+
+app.get("/",(req,res)=> {
+    res.status(200).json({msg:"success from api"})
+})
+
+app.listen(3000,() =>console.log("Server is running on port 3000"))
