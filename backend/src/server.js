9e99f4aad2eff2282a16782e6e05c4b08d4817a1
Rahcurl <rsharma194@rku.ac.in>
2025-12-13 21:07:29 +0545

    setting up for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
 import express from "express"
 import {ENV} from "./lib/env.js"
+import path from "path"
 
 const  app = express()
 
-console.log(ENV.PORT)
-console.log(ENV.DB_URL)
+const __dirname =path.resolve();
 
 //for getting the messange as json successfully 
 //use localhost:your_portnumber_in_dotenv i.e localhost:5001
@@ -12,4 +12,5 @@ app.get("/",(req,res)=> {
     res.status(200).json({msg:"api is up and running"})
 })
 
+//make our app ready for deploy
 app.listen(ENV.PORT,() =>console.log("Server is running on port:",ENV.PORT))
