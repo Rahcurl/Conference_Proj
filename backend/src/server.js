ed157262e1975408cf4c86e56ba0d2202c178fab
Rahcurl <rsharma194@rku.ac.in>
2025-12-23 10:11:45 +0545

    importing functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import { connectDB } from "./lib/db.js";
 import { start } from "repl";
 import cors from "cors"
 import {serve} from "inngest/express"
-import { inngest } from "./lib/inngest.js";
+import { inngest,functions } from "./lib/inngest.js";
 
 const app = express()
 
