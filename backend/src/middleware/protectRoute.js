7dab67b69d86879a89601a1920f58a271c481571
Rahcurl <rsharma194@rku.ac.in>
2025-12-26 00:09:01 +0545

    Package .json and protectedRoute file made

---
+++ backend/src/middleware/protectRoute.js
@@ -0,0 +1,25 @@
+import {requireAuth} from "@clerk/express";
+import user from "../models/User.js";
+
+export const protecRoute = [
+    requireAuth(),
+    async(req,res,next) => {
+        try{
+            const clerkId = req.auth().userId;
+            if(!clerkId) return res.status(401).json({msg:"Unauthorized - Invalid token"})
+
+                //find user in db by clerk Id
+                const user = await User.findOne({clerkId})
+                if(!user) return res.status(404).json({msg:"user not found"})
+                req.user = user
+            next()
+                
+        }
+        catch(error){
+            console.error("Error in protecRoute middleware",error);
+            res.status(500).json({msg:"Internal Server Error"})
+
+
+        }
+    }
+]
--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import { start } from "repl";
 import cors from "cors"
 import {serve} from "inngest/express"
 import { inngest,functions } from "./lib/inngest.js";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express()
 
@@ -15,11 +16,15 @@ app.use(express.json())
 // credentials:true meaning?? => server allows browser to include cookies on request
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 
+app.use(clerkMiddleware()); //this adds auth fieldsto request object: req.auth()
+
 app.use("/api/inngest",serve({client:inngest,functions  }))
+
 
 //for getting the messange as json successfully 
 //use localhost:your_portnumber_in_dotenv i.e localhost:5001
-app.get("/", (req, res) => { 
+app.get("/health", (req, res) => { 
+    req.auth()
     res.status(200).json({ msg: "api is up and running" })
 })
 
@@ -27,6 +32,10 @@ app.get("/books", (req, res) => {
     res.status(200).json({ msg: "this is the books endpoint" })
 })
 
+//when you pass an array of middlewareto express,it automatically flattens and executes them sequentially,one by one
+app.get("/video-calls",protecRoute, (req, res) => { 
+    res.status(200).json({ msg: "This is a protected route" })
+})
 //make our app ready for deployment
 if (ENV.NODE_ENV === "Production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
