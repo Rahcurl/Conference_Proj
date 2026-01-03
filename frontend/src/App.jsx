2780a8ce20a45cfe5874c0c53b630d49040d4ae5
Rahcurl <rsharma194@rku.ac.in>
2026-01-04 00:50:59 +0545

    Building Problem pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,39 @@
 import { 
-    SignedOut, 
-    SignedIn,      
-    SignInButton, 
-    SignOutButton, 
-    UserButton,
-    useUser
+    useUser 
 } from '@clerk/clerk-react';
-import { Routes,Route,Navigate } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import HomePage from './pages/HomePage';
-import ProblemsPage from './pages/ProblemsPage';
-import { Toaster } from 'react-hot-toast';
+import ProblemsPage from './pages/ProblemsPage';                                                                                                                                                                                                
 import DashboardPage from './pages/DashboardPage';
+import ProblemPage from './pages/ProblemPage'; // <-- ENSURE CAPITAL P
+import { Toaster } from 'react-hot-toast';
 
-  
 function App() {
+  const { isSignedIn, isLoaded } = useUser();
+  
+  if (!isLoaded) return null; 
 
-  const { isSignedIn, isLoaded} = useUser();
-  if(!isLoaded) return null; //reduce the flickering effect while refreshing and rendering
   return (
     <>
-    <Routes>
-    
-  <Route 
-    path="/" 
-    element={isSignedIn ? <Navigate to="/dashboard" /> : <HomePage />} 
-  />
-  <Route 
-    path="/dashboard" 
-    element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />} 
-  />
-  <Route 
-    path="/problems" 
-    element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />} 
-  />
-    </Routes>
-    <Toaster toastOptions={{duration:3000}}/>
+      <Routes>
+        <Route 
+          path="/" 
+          element={isSignedIn ? <Navigate to="/dashboard" /> : <HomePage />} 
+        />
+        <Route 
+          path="/dashboard" 
+          element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />} 
+        />
+        <Route 
+          path="/problems" 
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />} 
+        />
+        <Route 
+          path="/problem/:id" 
+          element={isSignedIn ? <ProblemPage /> : <Navigate to="/" />} 
+        />
+      </Routes>
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
   );
 }
--- frontend/src/pages/ProblemsPage.jsx
+++ frontend/src/pages/ProblemsPage.jsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+
 import Navbar from "../components/Navbar";
 
 import { PROBLEMS } from "../data/problems";
 import { ChevronRightIcon, Code2Icon } from "lucide-react";
 import { getDifficultyBadgeClass } from "../lib/utils";
+
 
 function ProblemsPage() {
   const problems = Object.values(PROBLEMS);
